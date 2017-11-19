d5941de0f39b428046b1df5f405f53791a735295
abocado <abo@adboomgrp.com>
2017-11-18 14:23:01 -1000

    this might take a few commits, Goose we gotta figure out local runs of expressjs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 const app = express();
 var PORT = process.env.PORT || 3000;
+var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + 'home.html'));
+    res.sendFile(path.join(__dirname + '/home.html'));
 });
 
 app.listen(PORT);

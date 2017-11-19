2a52d27a5a07016cfa334f51fdfa481cc3ab5af5
abocado <abo@adboomgrp.com>
2017-11-18 14:18:13 -1000

    getting the hang of expressjs, and i like it

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require("express");
+const app = express();
+var PORT = process.env.PORT || 3000;
 
-
-fs.readFile('./home.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(3000);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
 });
+
+app.listen(PORT);

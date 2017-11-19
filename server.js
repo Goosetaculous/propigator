3be03e5b258872a5e7c9101d24efea096942c7f2
abocado <abo@adboomgrp.com>
2017-11-18 16:09:41 -1000

    adding use static root path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 const app = express();
 var PORT = process.env.PORT || 3000;
 var path = require('path');
+app.use(express.static(path.join(__dirname + '/')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/home.html'));

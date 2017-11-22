f680114d99c10ea81dabf4640063e4d807dd4377
Goose <josephtrop@gmail.com>
2017-11-21 23:16:49 -0800

    added login

--- src/js/components/login/index.js
+++ src/js/components/login/index.js
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
 
+import FlatButton from 'material-ui/FlatButton';
 
 class Login extends Component {
     constructor() {
@@ -7,9 +10,32 @@ class Login extends Component {
     }
     render() {
         return (
-            <div style={{backgroundColor:"white"}}>
-                Login
-             </div>
+            <div
+                style={{
+                backgroundColor: "white",
+                width: "50%",
+                padding: "5px"
+            }}>
+                <Card style={{padding:"20px"}}>
+                    <CardHeader title="Log In" showExpandableButton={false}/>
+                    <TextField
+                        hintText="Email"                        
+                        floatingLabelText="Email"/>
+                    <br/>
+                    <TextField
+                        hintText="Password"                        
+                        type="password"
+                        floatingLabelText="Password"/>
+                    
+                    <CardActions>
+                        <FlatButton label="Login"/>
+                        
+                    </CardActions>
+                    <CardText expandable={false}>
+                        Best site ever.
+                    </CardText>
+                </Card>
+            </div>
         )
     }
 }

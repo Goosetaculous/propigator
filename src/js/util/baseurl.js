7e4013ba77b5229cf4a638a8ac37bdbee02bcbab
abocado <abo@adboomgrp.com>
2017-12-19 17:03:12 -1000

    Set base url back

--- src/js/util/baseurl.js
+++ src/js/util/baseurl.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
-var url = 'http://127.0.0.1:3000/';
-
+var url = process.env.NODE_ENV != 'development' ? 'http://api.propigator.com': 'http://127.0.0.1:3000/';
 var axiosInstance = axios.create({
     baseURL: url,
     headers: {"x-auth": process.env.PROPIGATOR_JWT_TOKEN || "Bearer JWT-Token"}
